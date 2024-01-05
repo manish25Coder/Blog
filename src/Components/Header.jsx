856ee3aea2e110039ce766b4365b280afe89bcc8
manish25Coder <manishsingh59766@gmail.com>
2024-01-05 16:16:53 +0530

    Added Navbar and css init

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+//imports react icons....
+import { FaBars } from "react-icons/fa";
+import { MdOutlineClose } from "react-icons/md";
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <nav>
+        <div className='container nav-container'>
+            <Link className='nav-logo' to={'/'}>
+                <h3>B<span>log Sit</span>e</h3>
+            </Link>
+
+            <ul className="nav-menu">
+                <li><Link to={'/profile'}>Profile</Link></li>
+                <li><Link to={'/create'}>Create Posts</Link></li>
+                <li><Link to={'/authors'}>Authors</Link></li>
+                <li><Link to={'/logout'}>Logout</Link></li>
+            </ul>
+
+            <button className='nav-toggle-button'>
+                <FaBars />
+                <MdOutlineClose/>
+            </button>
+        </div>
+    </nav>
   )
 }
