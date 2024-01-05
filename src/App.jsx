06bc1d12e5a1d69db786402133488d2da6f2186f
manish25Coder <manishsingh59766@gmail.com>
2024-01-05 14:29:20 +0530

    structuring the file

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div>App</div>
+  )
+}
---
+++ src/Pages/CategoryPost.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CategoryPost() {
+  return (
+    <div>CategoryPost</div>
+  )
+}
