c04c8ecff1c29bc5bf327b5695d9559b14dfc4f4
manish25Coder <manishsingh59766@gmail.com>
2024-01-06 11:57:36 +0530

    Added footer and css init

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+//Router link
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="container footer-container">
+      <ul className='footer-categories'>
+        <li><Link to={'/posts/categories/Education'}>Education</Link></li>
+        <li><Link to={'/posts/categories/Discovery'}>Discovery</Link></li>
+        <li><Link to={'/posts/categories/Agreculture'}>Agreculture</Link></li>
+        <li><Link to={'/posts/categories/Entertainment'}>Entertainment</Link></li>
+        <li><Link to={'/posts/categories/Music'}>Music</Link></li>
+        <li><Link to={'/posts/categories/Art'}>Art</Link></li>
+        <li><Link to={'/posts/categories/Business'}>Business</Link></li>
+        <li><Link to={'/posts/categories/Investment'}>Investment</Link></li>
+        <li><Link to={'/posts/categories/Fashion'}>Fashion</Link></li>
+        <li><Link to={'/posts/categories/Weather'}>Weather</Link></li>
+        <li><Link to={'/posts/categories/Sports'}>Sports</Link></li>
+        <li><Link to={'/posts/categories/Science'}>Science</Link></li>
+        
+      </ul>
+
+      <div className="footer-copyright">
+        <small> &copy; Copyright All Rights Reserved || manish25Coder</small>
+      </div>
+      </div>
+    </footer>
   )
 }
