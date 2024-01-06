2182c16e3e9e252d398ac2c9308ef416166e00b1
manish25Coder <manishsingh59766@gmail.com>
2024-01-06 12:16:32 +0530

    Added Error and css init

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function ErrorPage() {
   return (
-    <div>ErrorPage</div>
+    <section>
+      <div className="error-center">
+       <h2>404</h2>
+       <h3>Opps, This Page Not Found !</h3>
+       <Link to={'/'} className='btn btn-primary'>Back To Home</Link>
+      </div>
+    </section>
   )
 }
